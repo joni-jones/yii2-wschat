543b3a630baf40d1a10ecdf786b36080be17379d
Ievgen Sentiabov <jonik.lowkick@gmail.com>
2014-12-03 23:16:03 +0200

    Updated localization.

--- messages/en/app.php
+++ messages/en/app.php
@@ -2,4 +2,13 @@
 return [
     'User entity not found' => '',
     'Model class should implements `findOne()` method' => '',
+    'Chat Rooms' => '',
+    'Add' => '',
+    'History' => '',
+    'Exit' => '',
+    'Send' => '',
+    'Online Users' => '',
+    'Add User' => '',
+    'Close' => '',
+    'Username' => '',
 ];
