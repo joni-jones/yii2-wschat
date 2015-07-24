f85ea9efa2e5b45b79bbcf5a19a4b2e30c116106
joni-jones <joni-jones@users.noreply.github.com>
2015-07-24 21:29:02 +0300

    Fixed typo in column name

--- tests/codeception/_data/pgsql.sql
+++ tests/codeception/_data/pgsql.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS history CASCADE;
 CREATE TABLE history(
     id SERIAL,
     chat_id CHARACTER VARYING(60),
-    chat_name CHARACTER VARYING(60),
+    chat_title CHARACTER VARYING(60),
     user_id CHARACTER VARYING(60),
     username CHARACTER VARYING(60),
     avatar_16 CHARACTER VARYING(90),
